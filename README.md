69457b1f443ed720c040b64e576b240b5e345fa9
David Janes <davidjanes@davidjanes.com>
2016-06-07 15:57:10 -0400

    initial checkin

---
+++ README.md
@@ -0,0 +1,8 @@
+# iotdb-helpers
+IOTDB Module for [Helpers]().
+
+<img src="https://raw.githubusercontent.com/dpjanes/iotdb-homestar/master/docs/HomeStar.png" align="right" />
+
+# Installation
+
+[Install Home☆Star first](https://iotdb.io/about/install).
---
+++ helpers.js
@@ -0,0 +1,69 @@
+/*
+ *  helpers.js
+ *
+ *  David Janes
+ *  IOTDB.org
+ *  2013-12-01
+ *
+ *  Nodejs IOTDB control
+ *
+ *  Copyright [2013-2016] [David P. Janes]
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+"use strict";
+
+var crypto = require('crypto');
+var node_url = require('url');
+var path = require('path');
+
+exports.underscore = require('underscore')
+
+var modules = [
+    exports.underscore,
+    require('./lib/ld'),
+    require('./lib/id'),
+    require('./lib/d'),
+    require('./lib/hash'),
+    require('./lib/is'),
+    require('./lib/net'),
+    require('./lib/color'),
+    require('./lib/timestamp'),
+    require('./lib/error'),
+    require('./lib/convert'),
+    require('./lib/random'),
+    require('./lib/q'),
+    require('./lib/version'),
+    require('./lib/logger'),
+];
+for (var mi in modules) {
+    var module = modules[mi];
+    for (var key in module) {
+        exports[key] = module[key];
+    }
+}
+
+exports.queue = require('./lib/q').q.queue;
+exports.defaults = require('./lib/d').d.compose.shallow;
+exports.noop = function () {};
+exports.make_done = function (done) {
+    return function(value) {
+        done(null, value);
+    };
+};
+exports.make_error = function (done) {
+    return function(error) {
+        done(error);
+    };
+};
