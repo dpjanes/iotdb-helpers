3db1861896d5ebf04cdbb2163ae4fda87e43a0d9
David Janes <davidjanes@davidjanes.com>
2016-06-08 11:54:46 -0230

    more coverage

--- tests/test_coerce_classify.js
+++ tests/test_coerce_classify.js
@@ -26,36 +26,64 @@ const assert = require("assert");
 const coerce = require("../lib/coerce");
 
 describe("coerce", function() {
-    describe("coerce.classify", function() {
-        it("null", function() {
-            assert.strictEqual("iot:type.null", coerce.classify(null));
+    describe("classify", function() {
+        describe("good", function() {
+            it("null", function() {
+                assert.strictEqual("iot:type.null", coerce.classify(null));
+            });
+            it("boolean - false", function() {
+                assert.strictEqual("iot:type.boolean", coerce.classify(false));
+            });
+            it("boolean - true", function() {
+                assert.strictEqual("iot:type.boolean", coerce.classify(true));
+            });
+            it("integer - 0", function() {
+                assert.strictEqual("iot:type.integer", coerce.classify(0));
+            });
+            it("integer - negative", function() {
+                assert.strictEqual("iot:type.integer", coerce.classify(-34));
+            });
+            it("integer - positive", function() {
+                assert.strictEqual("iot:type.integer", coerce.classify(99));
+            });
+            it("number - negative", function() {
+                assert.strictEqual("iot:type.number", coerce.classify(-3.4));
+            });
+            it("number - positive", function() {
+                assert.strictEqual("iot:type.number", coerce.classify(99.9));
+            });
+            it("string - empty", function() {
+                assert.strictEqual("iot:type.string", coerce.classify(""));
+            });
+            it("string - empty", function() {
+                assert.strictEqual("iot:type.string", coerce.classify("not empty"));
+            });
         });
-        it("boolean - false", function() {
-            assert.strictEqual("iot:type.boolean", coerce.classify(false));
-        });
-        it("boolean - true", function() {
-            assert.strictEqual("iot:type.boolean", coerce.classify(true));
-        });
-        it("integer - 0", function() {
-            assert.strictEqual("iot:type.integer", coerce.classify(0));
-        });
-        it("integer - negative", function() {
-            assert.strictEqual("iot:type.integer", coerce.classify(-34));
-        });
-        it("integer - positive", function() {
-            assert.strictEqual("iot:type.integer", coerce.classify(99));
-        });
-        it("number - negative", function() {
-            assert.strictEqual("iot:type.number", coerce.classify(-3.4));
-        });
-        it("number - positive", function() {
-            assert.strictEqual("iot:type.number", coerce.classify(99.9));
-        });
-        it("string - empty", function() {
-            assert.strictEqual("iot:type.string", coerce.classify(""));
-        });
-        it("string - empty", function() {
-            assert.strictEqual("iot:type.string", coerce.classify("not empty"));
+        describe("unknown", function() {
+            it("undefined", function() {
+                assert.strictEqual(null, coerce.classify(undefined));
+            });
+            it("dictionary(empty)", function() {
+                assert.strictEqual(null, coerce.classify({}));
+            });
+            it("dictionary(full)", function() {
+                assert.strictEqual(null, coerce.classify({ a: 1 }));
+            });
+            it("array(empty)", function() {
+                assert.strictEqual(null, coerce.classify([]));
+            });
+            it("array(full)", function() {
+                assert.strictEqual(null, coerce.classify([ 1, 2, 3 ]));
+            });
+            it("function", function() {
+                assert.strictEqual(null, coerce.classify(() => {}));
+            });
+            it("class", function() {
+                assert.strictEqual(null, coerce.classify(Date));
+            });
+            it("object", function() {
+                assert.strictEqual(null, coerce.classify(new Date()));
+            });
         });
     });
 });
