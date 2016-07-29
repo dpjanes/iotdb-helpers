a28ebaf0894f0d868151847d93b02976b6ddc01f
David Janes <davidjanes@davidjanes.com>
2016-07-29 07:09:18 -0400

    data

--- test/test_cfg.js
+++ test/test_cfg.js
@@ -48,104 +48,104 @@ describe('test_cfg', function () {
         });
     });
     */
-    describe('cfg_find', function () {
+    describe('cfg.find', function () {
         it('all files in a directory', function () {
             var expects = ['data/cfg1/a.js', 'data/cfg1/a.json', 'data/cfg1/b.js', 'data/cfg1/c.json']
-            assert.ok(_.is.Equal(expects, _.cfg.find("data/cfg1")))
-            assert.ok(_.is.Equal(expects, _.cfg.find("data/cfg1:data/cfg3")))
-            assert.ok(_.is.Equal(expects, _.cfg.find(["data/cfg1"])))
-            assert.ok(_.is.Equal(expects, _.cfg.find(["data/cfg1", "data/cfg3"])))
+            assert.deepEqual(expects, _.cfg.find(".", "data/cfg1"))
+            assert.deepEqual(expects, _.cfg.find(".", "data/cfg1:data/cfg3"))
+            assert.deepEqual(expects, _.cfg.find(".", ["data/cfg1"]))
+            assert.deepEqual(expects, _.cfg.find(".", ["data/cfg1", "data/cfg3"]))
         })
         it('expansion', function () {
             /*
             var expects = ['data/cfg1/a.js', 'data/cfg1/a.json', 'data/cfg1/b.js', 'data/cfg1/c.json']
-            assert.ok(_.is.Equal(expects, _.cfg.find("$TEST_DATA/cfg1")))
-            assert.ok(_.is.Equal(expects, _.cfg.find("$TEST_DATA/cfg1:$TEST_DATA/cfg3")))
-            assert.ok(_.is.Equal(expects, _.cfg.find(["$TEST_DATA/cfg1"])))
-            assert.ok(_.is.Equal(expects, _.cfg.find(["$TEST_DATA/cfg1", "$TEST_DATA/cfg3"])))
+            assert.deepEqual(expects, _.cfg.find("$TEST_DATA/cfg1"))
+            assert.deepEqual(expects, _.cfg.find("$TEST_DATA/cfg1:$TEST_DATA/cfg3")))
+            assert.deepEqual(expects, _.cfg.find(["$TEST_DATA/cfg1"]))
+            assert.deepEqual(expects, _.cfg.find(["$TEST_DATA/cfg1", "$TEST_DATA/cfg3"]))
             */
         })
         it('.max', function () {
             var expects = ['data/cfg1/a.js', 'data/cfg1/a.json', ]
-            assert.ok(_.is.Equal(expects, _.cfg.find("data/cfg1", null, {
+            assert.deepEqual(expects, _.cfg.find("data/cfg1", null, {
                 max: 2
-            })))
+            }))
 
             var expects = ['data/cfg1/a.js', 'data/cfg1/a.json', 'data/cfg1/b.js', 'data/cfg1/c.json']
-            assert.ok(_.is.Equal(expects, _.cfg.find("data/cfg1", null, {
+            assert.deepEqual(expects, _.cfg.find("data/cfg1", null, {
                 max: 4
-            })))
-            assert.ok(_.is.Equal(expects, _.cfg.find("data/cfg1", null, {
+            }))
+            assert.deepEqual(expects, _.cfg.find("data/cfg1", null, {
                 max: 10
-            })))
-            assert.ok(_.is.Equal(expects, _.cfg.find("data/cfg1", null, {
+            }))
+            assert.deepEqual(expects, _.cfg.find("data/cfg1", null, {
                 max: 0
-            })))
+            }))
         })
         it('.dotfiles', function () {
             var expects = ['data/cfg1/a.js', 'data/cfg1/a.json', 'data/cfg1/b.js', 'data/cfg1/c.json']
-            assert.ok(_.is.Equal(expects, _.cfg.find("data/cfg1", null, {
+            assert.deepEqual(expects, _.cfg.find("data/cfg1", null, {
                 dotfiles: false
-            })))
+            }))
 
             var expects = [
                 'data/cfg1/.d', 'data/cfg1/.d.js', 'data/cfg1/.d.json', 'data/cfg1/a.js',
                 'data/cfg1/a.json', 'data/cfg1/b.js', 'data/cfg1/c.json'
             ]
-            assert.ok(_.is.Equal(expects, _.cfg.find("data/cfg1", null, {
+            assert.deepEqual(expects, _.cfg.find("data/cfg1", null, {
                 dotfiles: true
-            })))
+            }))
         })
         it('name', function () {
             var expects = ['data/cfg1/a.js']
-            assert.ok(_.is.Equal(expects, _.cfg.find("data/cfg1", "a.js")))
+            assert.deepEqual(expects, _.cfg.find("data/cfg1", "a.js"))
 
             var expects = []
-            assert.ok(_.is.Equal(expects, _.cfg.find("data/cfg1", "doesnotexist.js")))
+            assert.deepEqual(expects, _.cfg.find("data/cfg1", "doesnotexist.js"))
         })
         it('regex', function () {
             var expects = ['data/cfg1/a.js', 'data/cfg1/b.js']
-            assert.ok(_.is.Equal(expects, _.cfg.find("data/cfg1", /[.]js$/)))
+            assert.deepEqual(expects, _.cfg.find("data/cfg1", /[.]js$/))
 
             var expects = ['data/cfg1/.d.js', 'data/cfg1/a.js', 'data/cfg1/b.js']
-            assert.ok(_.is.Equal(expects, _.cfg.find("data/cfg1", /[.]js$/, {
+            assert.deepEqual(expects, _.cfg.find("data/cfg1", /[.]js$/, {
                 dotfiles: true
-            })))
+            }))
 
             var expects = ['data/cfg1/.d.js']
-            assert.ok(_.is.Equal(expects, _.cfg.find("data/cfg1", /[.]js$/, {
+            assert.deepEqual(expects, _.cfg.find("data/cfg1", /[.]js$/, {
                 dotfiles: true,
                 max: 1
-            })))
+            }))
         })
         it('all up - multiple directories', function () {
             /*
             var expects = ['data/cfg1/a.js', 'data/cfg1/b.js', 'data/cfg2/b.js', 'data/cfg2/e.js']
             var gots = _.cfg.find("$TEST_DATA/cfg1:$TEST_DATA/cfg2", /[.]js$/)
-            assert.ok(_.is.Equal(expects, gots))
+            assert.deepEqual(expects, gots)
 
             var expects = ['data/cfg1/b.js', 'data/cfg2/b.js', ]
             var gots = _.cfg.find("$TEST_DATA/cfg1:$TEST_DATA/cfg2", "b.js")
-            assert.ok(_.is.Equal(expects, gots))
+            assert.deepEqual(expects, gots)
 
             var expects = ['data/cfg1/b.js']
             var gots = _.cfg.find("$TEST_DATA/cfg1:$TEST_DATA/cfg2", "b.js", {
                 max: 1
             })
-            assert.ok(_.is.Equal(expects, gots))
+            assert.deepEqual(expects, gots)
 
             var expects = ['data/cfg1/.d.js']
             var gots = _.cfg.find("$TEST_DATA/cfg1:$TEST_DATA/cfg2", /[.]js$/, {
                 max: 1,
                 dotfiles: true
             })
-            assert.ok(_.is.Equal(expects, gots))
+            assert.deepEqual(expects, gots)
             */
         })
         it('search subdirectories', function () {
             var expects = ['data/cfg3/sub1/file1.txt', 'data/cfg3/sub2/file2.txt'];
             var gots = _.cfg.find("data/cfg3//")
-            assert.ok(_.is.Equal(expects, gots))
+            assert.deepEqual(expects, gots)
         })
     })
     describe('cfg.load.json', function () {
@@ -164,7 +164,7 @@ describe('test_cfg', function () {
                             "name": "c.json",
                             "data": [1, 2, 3]
                         }
-                        assert.ok(_.is.Equal(expect, paramd.doc))
+                        assert.deepEqual(expect, paramd.doc)
                     } else {
                         assert.ok(false);
                     }
