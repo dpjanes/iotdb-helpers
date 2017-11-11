f3770fba082f01c9104dd3b2d6d6fcdeb2af4a6e
David Janes <david@consensas.com>
2017-11-11 13:11:00 -0500

    add a pile of test cases

--- test/test_promise_series.js
+++ test/test_promise_series.js
@@ -33,14 +33,13 @@ describe("promise", function() {
 
         })
     })
-    describe("good", function() {
+    describe("good method", function() {
         it("method is a function", function(done) {
             _.promise.make({
                 values: [ 1, 2, 3, 4, 5 ],
             })
                 .then(_.promise.series({
                     method: sd => sd.value * sd.value,
-                    // inputs: sd => sd.values.map(value => ({ value: value })),
                     inputs: "values:value",
                     output_selector: result => result,
                     output_filter: result => result != 9,
@@ -67,6 +66,25 @@ describe("promise", function() {
                     method: _promise,
                     inputs: "values:value",
                     output_selector: sd => sd.value,
+                    output_filter: result => result != 9,
+                    outputs: "squares",
+                }))
+                .then(sd => {
+                    assert.deepEqual(sd.squares, [ 1, 4, 16, 25 ])
+                    done()
+                })
+                .catch(done)
+        })
+    })
+    describe("good inputs", function() {
+        it("method is a string", function(done) {
+            _.promise.make({
+                values: [ 1, 2, 3, 4, 5 ],
+            })
+                .then(_.promise.series({
+                    method: sd => sd.value * sd.value,
+                    inputs: "values:value",
+                    output_selector: result => result,
                     output_filter: result => result != 9,
                     outputs: "squares",
                 }))
@@ -122,6 +140,79 @@ describe("promise", function() {
                 }))
                 .then(sd => {
                     assert.deepEqual(sd.squares, [ 1, 4, 16, 25 ])
+                    done()
+                })
+                .catch(done)
+        })
+    })
+    describe("good inputs - with input filter", function() {
+        it("method is a string", function(done) {
+            _.promise.make({
+                values: [ 1, 2, 3, 4, 5 ],
+            })
+                .then(_.promise.series({
+                    method: sd => sd.value * sd.value,
+                    inputs: "values:value",
+                    input_filter: value => value != 2,
+                    output_selector: result => result,
+                    output_filter: result => result != 9,
+                    outputs: "squares",
+                }))
+                .then(sd => {
+                    assert.deepEqual(sd.squares, [ 1, 16, 25 ])
+                    done()
+                })
+                .catch(done)
+        })
+        it("inputs is an Array (with self.input_field)", function(done) {
+            _.promise.make({
+            })
+                .then(_.promise.series({
+                    method: sd => sd.value * sd.value,
+                    inputs: [ 1, 2, 3, 4, 5 ],
+                    input_field: "value",
+                    input_filter: value => value != 2,
+                    output_selector: result => result,
+                    output_filter: result => result != 9,
+                    outputs: "squares",
+                }))
+                .then(sd => {
+                    assert.deepEqual(sd.squares, [ 1, 16, 25 ])
+                    done()
+                })
+                .catch(done)
+        })
+        it("inputs is function (with self.input_field)", function(done) {
+            _.promise.make({
+            })
+                .then(_.promise.series({
+                    method: sd => sd.value * sd.value,
+                    inputs: sd => [ 1, 2, 3, 4, 5 ].map(value => ({ value: value })),
+                    input_filter: sd => sd.value != 2,
+                    output_selector: result => result,
+                    output_filter: result => result != 9,
+                    outputs: "squares",
+                }))
+                .then(sd => {
+                    assert.deepEqual(sd.squares, [ 1, 16, 25 ])
+                    done()
+                })
+                .catch(done)
+        })
+        it("inputs is function (without self.input_field)", function(done) {
+            _.promise.make({
+            })
+                .then(_.promise.series({
+                    method: sd => sd.value * sd.value,
+                    inputs: sd => [ 1, 2, 3, 4, 5 ],
+                    input_filter: value => value != 2,
+                    input_field: "value",
+                    output_selector: result => result,
+                    output_filter: result => result != 9,
+                    outputs: "squares",
+                }))
+                .then(sd => {
+                    assert.deepEqual(sd.squares, [ 1, 16, 25 ])
                     done()
                 })
                 .catch(done)
