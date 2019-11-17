fc5ff2af8856d1b2f6c2c830ed8945cbb496c244
David Janes <david@consensas.com>
2019-11-17 07:01:10 -0500

    expand test cases

--- test/test_promise_done.js
+++ test/test_promise_done.js
@@ -12,14 +12,14 @@ const _ = require("..")
 const assert = require("assert")
 
 describe("promise/done", function() {
-    describe("good", function() {
-        const original = {
-            "value": 10,
-            "json": {
-                "value": 15,
-            },
-        } 
+    const original = {
+        "value": 10,
+        "json": {
+            "value": 15,
+        },
+    } 
 
+    describe("old-way", function() {
         it("sd", function(done) {
             const my_done = (error, self) => {
                 assert.deepEqual(self.value, original.value)
@@ -137,6 +137,117 @@ describe("promise/done", function() {
             _.promise.make(original)
                 .then(_.promise.done(my_done, original, "json:njson,value:nvalue"))
                 .catch(done)
+        })
+    })
+    describe("new-way", function() {
+        it("sd", function(done) {
+            const my_done = (error, self) => {
+                assert.deepEqual(self.value, original.value)
+                assert.deepEqual(self.json, original.json)
+
+                done(error)
+            }
+
+            _.promise(original)
+                .end(my_done)
+        })
+        it("sd with change", function(done) {
+            const my_done = (error, self) => {
+                assert.deepEqual(self.value, 20)
+                assert.deepEqual(self.json, original.json)
+
+                done(error)
+            }
+
+            _.promise(original)
+                .add("value", 20)
+                .end(my_done)
+        })
+        it("self", function(done) {
+            const my_done = (error, self) => {
+                assert.deepEqual(self.value, original.value)
+                assert.deepEqual(self.json, original.json)
+
+                done(error)
+            }
+
+            _.promise(original)
+                .end(my_done, original)
+        })
+        it("self with change", function(done) {
+            const my_done = (error, self) => {
+                assert.deepEqual(self.value, original.value)
+                assert.deepEqual(self.json, original.json)
+
+                done(error)
+            }
+
+            _.promise(original)
+                .add("value", 20)
+                .end(my_done, original)
+        })
+        it("self with variable", function(done) {
+            const my_done = (error, self) => {
+                assert.deepEqual(self.value, 30); // original.value)
+                assert.deepEqual(self.json, original.json)
+
+                done(error)
+            }
+
+            _.promise(original)
+                .add("value", 30)
+                .end(my_done, original, "value")
+        })
+        it("self with nested variable", function(done) {
+            const my_done = (error, self) => {
+                assert.deepEqual(self.value, original.json.value)
+                assert.deepEqual(self.json, original.json)
+
+                done(error)
+            }
+
+            _.promise(original)
+                .add("value", 30)
+                .end(my_done, original, "json/value")
+        })
+        it("self with nested variable and rename", function(done) {
+            const my_done = (error, self) => {
+                assert.deepEqual(self.value, original.value)
+                assert.deepEqual(self.v2, original.json.value)
+                assert.deepEqual(self.json, original.json)
+
+                done(error)
+            }
+
+            _.promise(original)
+                .add("value", 30)
+                .end(my_done, original, "json/value:v2")
+        })
+        it("self with multiple arguments", function(done) {
+            const my_done = (error, self) => {
+                assert.deepEqual(self.value, original.value)
+                assert.deepEqual(self.nvalue, original.value)
+                assert.deepEqual(self.json, original.json)
+                assert.deepEqual(self.njson, original.json)
+
+                done(error)
+            }
+
+            _.promise(original)
+                .end(my_done, original, "json:njson", "value:nvalue")
+        })
+        it("self with comma arguments", function(done) {
+            const my_done = (error, self) => {
+                assert.deepEqual(self.value, original.value)
+                assert.deepEqual(self.nvalue, original.value)
+                assert.deepEqual(self.json, original.json)
+                assert.deepEqual(self.njson, original.json)
+
+                done(error)
+            }
+
+            _.promise(original)
+                .end(my_done, original, "json:njson,value:nvalue")
         })
     })
 })
