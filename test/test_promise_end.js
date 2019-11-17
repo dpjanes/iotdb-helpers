52b2bc5f6b6475f511b96a05a9e1cc3d19052763
David Janes <david@consensas.com>
2019-11-17 07:49:21 -0500

    test validation

--- test/test_promise_end.js
+++ test/test_promise_end.js
@@ -18,44 +18,122 @@ describe("promise/end", function() {
             "value": 15,
         },
     } 
+    const JSON = original
+    const A = 122
+    const B = "Hello, World"
 
-    describe("good", function() {
-        it("works", function(done) {
-            const JSON = original
-            const A = 122
-            const B = "Hello, World"
 
-            const _wrap_done = (error, self) => {
-                if (error) {
-                    return done(error)
-                }
-
-                try {
-                    assert.deepEqual(self.json, JSON)
-                    assert.strictEqual(self.a, A)
-                    assert.strictEqual(self.b, B)
-                } catch (x) {
-                    return done(x)
-                }
-
-                done(null)
+    it("works", function(done) {
+        const _wrap_done = (error, self) => {
+            if (error) {
+                return done(error)
             }
 
-            const self = {}
-            const something = () => {}
-            something.produces = {
-                json: _.is.JSON,
-                a: _.is.Number,
-                b: _.is.String,
+            try {
+                assert.deepEqual(self.json, JSON)
+                assert.strictEqual(self.a, A)
+                assert.strictEqual(self.b, B)
+            } catch (x) {
+                return done(x)
             }
 
-            _.promise(self)
-                .make(sd => {
-                    sd.json = JSON
-                    sd.a = A
-                    sd.b = B
-                })
-                .end(_wrap_done, self, something)
-        })
+            done(null)
+        }
+
+        const self = {}
+        const something = () => {}
+        something.produces = {
+            json: _.is.JSON,
+            a: _.is.Number,
+            b: _.is.String,
+        }
+
+        _.promise(self)
+            .make(sd => {
+                sd.json = JSON
+                sd.a = A
+                sd.b = B
+            })
+            .end(_wrap_done, self, something)
+    })
+    it("allows nulls", function(done) {
+        const _wrap_done = (error, self) => {
+            if (error) {
+                return done(error)
+            }
+
+            try {
+                assert.strictEqual(self.json, null)
+                assert.strictEqual(self.a, null)
+                assert.strictEqual(self.b, null)
+            } catch (x) {
+                return done(x)
+            }
+
+            done(null)
+        }
+
+        const self = {}
+        const something = () => {}
+        something.produces = {
+            json: _.is.JSON,
+            a: _.is.Number,
+            b: _.is.String,
+        }
+
+        _.promise(self)
+            .make(sd => {
+                sd.json = null
+                sd.a = null
+                sd.b = null
+            })
+            .end(_wrap_done, self, something)
+    })
+    it("undefined is exception", function(done) {
+        const _wrap_done = (error, self) => {
+            if (error) {
+                return done(null)
+            }
+
+            done(new Error("expected an exception"))
+        }
+
+        const self = {}
+        const something = () => {}
+        something.produces = {
+            json: _.is.JSON,
+            a: _.is.Number,
+            b: _.is.String,
+        }
+
+        _.promise(self)
+            .make(sd => {
+            })
+            .end(_wrap_done, self, something)
+    })
+    it("bad value is exception", function(done) {
+        const _wrap_done = (error, self) => {
+            if (error) {
+                return done(null)
+            }
+
+            done(new Error("expected an exception"))
+        }
+
+        const self = {}
+        const something = () => {}
+        something.produces = {
+            json: _.is.JSON,
+            a: _.is.Number,
+            b: _.is.String,
+        }
+
+        _.promise(self)
+            .make(sd => {
+                sd.json = JSON
+                sd.a = A
+                sd.b = 839389
+            })
+            .end(_wrap_done, self, something)
     })
 })
